ec51f2f54f71617e8617104aaa85aeabdda0d901
Artur <wildson2585@gmail.com>
2017-01-23 21:45:41 +0300

    ch9

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
+var jqupload = require('jquery-file-upload-middleware');
+var credentials = require('./credentials.js');
+
 
 var app = express();
 
@@ -64,7 +68,62 @@ app.use(function(req, res, next){
     next();
 });
 
+// jQuery File Upload endpoint middleware
+app.use('/upload', function(req, res, next){
+    var now = Date.now();
+    jqupload.fileHandler({
+        uploadDir: function(){
+            return __dirname + '/public/uploads/' + now;
+        },
+        uploadUrl: function(){
+            return '/uploads/' + now;
+        },
+    })(req, res, next);
+});
+
 app.use(require('body-parser').urlencoded({ extended: true }));
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
+app.get('/newsletter', function(req, res) {
+    // CSRF to be learned later
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+/*app.post('/process', function(req, res) {
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field: ' + req.body._csrf);
+    console.log('Name (from visible form field: ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
+});*/
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json,html') === 'json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+
+app.get('/contest/vacation-photo', function(req, res) {
+    var now = new Date();
+    res.render('contest/vacation-photo', {
+        year: now.getFullYear(),
+        month: now.getMonth()
+    });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
 
 app.get('/', function(req, res) {
     res.render('home');
