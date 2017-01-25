dc97ad86c0bed23f34a0c7467e0791ea58da8551
Artur <wildson2585@gmail.com>
2017-01-25 23:55:28 +0300

    ch10

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,8 @@ var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
 var jqupload = require('jquery-file-upload-middleware');
 var credentials = require('./credentials.js');
+var cartValidation = require('./lib/cartValidation.js');
+var connect = require('connect');
 
 
 var app = express();
@@ -30,6 +32,19 @@ app.use(function(req, res, next){
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
     next();
+});
+
+app.use(function(req, res, next){
+    console.log('обработка запроса для "' + req.url + '"...');
+    next();
+});
+app.use(function(req, res, next) {
+    console.log('завершаем запрос');
+    res.send('Спасибо за игру!');
+    next();
+});
+app.use(function(req, res, next) {
+    console.log('wont be called');
 });
 
 // mocked weather data
@@ -84,6 +99,19 @@ app.use('/upload', function(req, res, next){
 app.use(require('body-parser').urlencoded({ extended: true }));
 
 app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret
+}));
+
+app.use(function(req, res, next){
+    //Если имеется экстр. сообщение
+    //поместим его в контекст, а затем удалим
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+});
 
 app.get('/newsletter', function(req, res) {
     // CSRF to be learned later
@@ -159,6 +187,51 @@ app.get('/tours/oregon-coast', function(req, res){
 app.get('/tours/request-group-rate', function(req, res){
     res.render('tours/request-group-rate');
 });
+
+// for now, we're mocking NewsletterSignup:
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+    cb();
+};
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res){
+    var name = req.body.name || '', email = req.body.email || '';
+    // input validation
+    if(!email.match(VALID_EMAIL_REGEX)) {
+        if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was  not valid.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+    new NewsletterSignup({ name: name, email: email }).save(function(err){
+        if(err) {
+            if(req.xhr) return res.json({ error: 'Database error.' });
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Database error!',
+                message: 'There was a database error; please try again later.',
+            };
+            return res.redirect(303, '/newsletter/archive');
+        }
+        if(req.xhr) return res.json({ success: true });
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'You have now been signed up for the newsletter.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    });
+});
+
+// cartValidation
+app.use(cartValidation.checkWaivers);
+app.use(cartValidation.checkGuestCounts);
 
 //404
 app.use(function(req, res) {
